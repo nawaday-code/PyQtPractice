c5e101e260a260839ddeaf4188311842b3dfe370
nawaday <u.nawaday@gmail.com>
2022-12-18 21:18:29 +0900

    sorted

--- tableView_usingModel.py
+++ tableView_usingModel.py
@@ -1,17 +1,18 @@
 import sys
 # from PyQt5.QtCore import (Qt, QModelIndex, QAbstractTableModel)
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtGui import *
 
-from assignMember import *
-
+from dataContainer import *
 
 
+# dataframeを受け取って表示するようにする
+# roleをeditableにして編集してみる
 
 class TableViewTestModel(QtCore.QAbstractTableModel):
-    def __init__(self, members: list[Person], parent=None):
+    def __init__(self, shiftInfo: CreateShiftInfo, parent=None):
         super().__init__(parent)
-        self.members = members
+        self.members = shiftInfo.members
+        self.shiftInfo = shiftInfo
 
     def data(self, index: QtCore.QModelIndex, role: int):
         if role == QtCore.Qt.EditRole or role == QtCore.Qt.ItemDataRole.DisplayRole:
@@ -28,10 +29,11 @@ class TableViewTestModel(QtCore.QAbstractTableModel):
     def headerData(self, section: int, orientation: QtCore.Qt.Orientation, role: int = QtCore.Qt.ItemDataRole.DisplayRole):
         if role == QtCore.Qt.ItemDataRole.DisplayRole:
             if orientation == QtCore.Qt.Orientation.Horizontal:
-                return Person.toHeader()
+                return self.shiftInfo.toHeader()
+
 
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, members: list[Person]):
+    def __init__(self, shiftInfo: CreateShiftInfo):
         super().__init__()
         rootWidget = QtWidgets.QWidget()
         layout = QtWidgets.QGridLayout()
@@ -40,28 +42,15 @@ class MainWindow(QtWidgets.QMainWindow):
         layout.addWidget(self.table)
         rootWidget.setLayout(layout)
         self.setCentralWidget(rootWidget)
-        
+
         self.resize(1500, 800)
 
-        self.table.setModel(TableViewTestModel(members))
-
-
-
-ShiftDataReader.readConfigvar('radschedule\勤務表\data\configvar.dat')
-members = ShiftDataReader.readStaffInfo('radschedule\勤務表\data\staffinfo.dat')
-ShiftDataReader.applyShift2Member('radschedule\勤務表\data\shift.dat', members)
-
-
+        self.table.setModel(TableViewTestModel(shiftInfo))
 
 
 app = QtWidgets.QApplication(sys.argv)
-
-testWindow = MainWindow(members)
+shiftInfo = CreateShiftInfo('data')
+testWindow = MainWindow(shiftInfo)
 testWindow.show()
 
 sys.exit(app.exec_())
-
-
-
-
-
